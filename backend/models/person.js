ec2869a809469097e860e25b1c2842e66888f083
Szymon Cybulski <szymcyb04@protonmail.com>
2025-09-09 14:57:39 +0100

    add: validation to phone number

--- backend/models/person.js
+++ backend/models/person.js
@@ -11,14 +11,26 @@ mongoose.connect(url)
         console.log("error connecting to MongoDB: ", error.message)
     });
 
+const numberValidator = (number) => {
+    return /\d{3}-\d{3}-\d{4}/.test(number);
+}
+
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
+        minLength: 3,
         required: true
     },
     number: {
         type: String,
-        required: true
+        required: true,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{3}-\d{3}-\d{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
     }
 });
 
